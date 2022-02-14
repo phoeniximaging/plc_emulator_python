8ec0ffd709987e3dfcc2f38f8bbf1203dead3f89
Howard Roush <howard.roush@phoeniximaging.com>
2022-02-14 11:20:38 -0500

    Added in logic to go through initial three steps of timing diagram. Need to modify with companion trigger program to correctly set tags from each side (source).

--- plc_emulator.py
+++ plc_emulator.py
@@ -3,6 +3,12 @@ import json
 import time
 import os
 
+'''
+This branch of the plc_emulator goes iterates through the boolean tags of the 'Robot 2 Load Program Parameters' timing diagram.
+This begins with 'READY' high (1). 'READY' is dropped (0) and 'LOAD_PROGRAM' is set high (1), this is where mirroring data back would happen.
+Then 'READY' is set back high (1) and 'LOAD_PROGRAM' is dropped low (0), representing that the correct part information has been passed to the
+Keyence and we are now ready to trigger/scan.
+'''
 
 def csv_read():
     #csv file reader variable declaration
@@ -46,24 +52,42 @@ def csv_write(results):
 
 #Reads in .csv (pretend PLC), sets 'START_PROGRAM' to 1, waits 30 seconds, sets to 0, waits 30 seconds, repeats forever
 def main():
-    x = 30 #seconds variable for artificial pause
+    x = 10 #seconds variable for artificial pause
     while(True):
         csv_results = csv_read()
         #print(csv_results)
-        #set START_PROGRAM high then wait 30 seconds (triggering Keyence)
-        csv_results['START_PROGRAM'] = 1
-        print('Writing: \'START_PROGRAM\' : 1')
+
+        # Stage 1 : Load Program Parameters
+        # READY = 1, other boolean flags = 0
+        print('\nStage 1 : Load')
+        csv_results['READY'] = 1
+        print('Writing: \'READY\' : 1')
         csv_write(csv_results)
         print(f'Write Complete! Waiting {x} seconds...')
         time.sleep(x)
 
-        #set START_PROGRAM low then wait 30 seconds
-        csv_results['START_PROGRAM'] = 0
-        print('Writing: \'START_PROGRAM\' : 0')
+        # Stage 2 : Load
+        # READY = 0, LOAD_PROGRAM = 1, mirror data
+        print('\nStage 2: Load')
+        csv_results['READY'] = 0
+        csv_results['LOAD_PROGRAM'] = 1
+        print('Writing \'READY\' : 0 ; \'LOAD_PROGRAM\' : 1')
+        # This is where specific data would be mirrored back to a real PLC (part info, date/time)
+        print('!Data Mirrored Here!')
         csv_write(csv_results)
         print(f'Write Complete! Waiting {x} seconds...')
         time.sleep(x)
 
+        # Stage 3 : Load
+        # READY = 1, LOAD_PROGRAM = 0, "mirroring data" complete
+        print('\nStage 3 : Load')
+        csv_results['READY'] = 1
+        csv_results['LOAD_PROGRAM'] = 0
+        print('Writing \'READY\' : 1 ; \'LOAD_PROGRAM\' : 0')
+        csv_write(csv_results)
+        print(f'Write Complete! Waiting {x} seconds...')
+        time.sleep(x)
+#END main
 
 if __name__ == '__main__':
     main()
